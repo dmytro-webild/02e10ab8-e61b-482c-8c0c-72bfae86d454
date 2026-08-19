e41af851074bb28ab7b88ac4019fa5e2f90ef09c
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-08-19 17:18:09 +0000

    Bob AI: Updated Jontrell's title to Director of Security

--- src/pages/HomePage/sections/Team.tsx
+++ src/pages/HomePage/sections/Team.tsx
@@ -14,7 +14,7 @@ const members = [
   },
   {
     name: "Jontrell Taylor",
-    role: "Client Relations Manager",
+    role: "Director of Security",
     imageSrc: "https://storage.googleapis.com/webild/users/user_3HymNublRvuFtoKZ5cOUF2ujXj0/uploaded-1786909729808-4ywopy8p.jpg"
   },
   {
